bbd296e0f9d4175dc96ff7e57fdf304712b14b0f
VI25TO01 <85112150+VI25TO01@users.noreply.github.com>
2021-06-01 00:26:36 +0400

    Add files via upload

---
+++ MailAddress.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWorkOne
+{
+    class MailAddress
+    {
+        string organisationName;
+        string countryName;
+        string cityName;
+        string streetName;
+        int houseNumber;
+        int mailIndex;
+
+        public string OrganisationName
+        {
+            get
+            {
+                return organisationName;
+            }
+            set
+            {
+                if (value == null || value.Length == 0)
+                    throw new Exception("Неподходящее название компании");
+                else
+                    organisationName = value;
+            }
+        }
+        public string CountryName
+        {
+            get
+            {
+                return countryName;
+            }
+            set
+            {
+                if (value == null || value.Length == 0)
+                    throw new Exception("Неподходящее название страны");
+                else
+                    countryName = value;
+            }
+        }
+        public string CityName
+        {
+            get
+            {
+                return cityName;
+            }
+            set
+            {
+                if (value == null || value.Length == 0)
+                    throw new Exception("Неподходящее название города");
+                else
+                    cityName = value;
+            }
+        }
+        public string StreetName
+        {
+            get
+            {
+                return streetName;
+            }
+            set
+            {
+                if (value == null || value.Length == 0)
+                    throw new Exception("Неподходящее название улицы");
+                else
+                    streetName = value;
+            }
+        }
+
+        public string HouseNumber
+        {
+            get
+            {
+                return houseNumber;
+            }
+            set
+            {
+                if (value = 0)
+                    throw new Exception("Неподходящее значение ");
+                else
+                    houseNumber = value;
+            }
+        }
+
+        public string Index
+        {
+            get
+            {
+                return mailIndex;
+            }
+            set
+            {
+                if (value <= 0 )
+                    throw new Exception("Неподходящее значение");
+                else
+                    mailIndex = value;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWorkOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MailAddress mail = new MailAddress();
+                Console.WriteLine("Название вашей организации:");
+                mail.OrganisationName = Console.ReadLine();
+                Console.WriteLine("Почтовый индекс вашей организации: ");
+                mail.Index = int.Parse(Console.ReadLine());
+                Console.WriteLine("В каком стране находитсяваша организация:");
+                mail.CountryName = Console.ReadLine();
+                Console.WriteLine("В каком городе она находится?");
+                mail.CityName = Console.ReadLine();
+                Console.WriteLine("Укажите на какой улице находится ваша организация:");
+                mail.StreetName = Console.ReadLine();
+                Console.WriteLine("Укажите номер дома:");
+                mail.HouseNumber = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ваша организация: \n");
+                Console.WriteLine("Название: " + mail.OrganisationName);
+                Console.WriteLine("Почтовый индекс: " + mail.Index);
+                Console.WriteLine("Страна: " + mail.CountryName);
+                Console.WriteLine("Город: " + mail.CityName);
+                Console.WriteLine("Улица, дом: " + mail.StreetName + ", " + mail.HouseNumber);
+            }
+            catch (Exception)
+        
+            {
+                Console.WriteLine("Неверно задано значение!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
